2a5d9320dcaa43ccec94fbad14e563442fe25da8
Caio Batista <cbatista.93@gmail.com>
2017-04-29 15:45:00 -0300

    color, flex justify content center & imgs

--- application/views/header.php
+++ application/views/header.php
@@ -1,16 +1,17 @@
 <div class="header">
   <!-- This section gets pushed to the left side-->
   <div class="header__section">
+
     <div class="header__item header-logo">
-      <div class="fa fa-inbox"></div>
+      <img src="public/img/logo-branco.png" height="100" alt="">
     </div>
-    <div class="header__item header-button is-site-header-item-selected">Inbox</div>
+    <!-- <div class="header__item header-button is-site-header-item-selected">Inbox</div>
     <div class="header__item header-button">Sent</div>
-    <div class="header__item header-button">Trash</div>
+    <div class="header__item header-button">Trash</div> -->
   </div>
   <!-- This section gets pushed to the right side-->
-  <div class="header__section">
+  <!-- <div class="header__section">
     <div class="header__item header-button">Settings</div>
     <div class="header__item header-button">Log out</div>
-  </div>
+  </div> -->
 </div>
