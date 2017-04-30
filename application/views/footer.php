57f7eb74923332b8bf87e4463cc1f87f3c7e9df2
Caio Batista <cbatista.93@gmail.com>
2017-04-29 21:04:43 -0300

    style upgrade & home component

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,36 @@
       <!-- Footer -->
       <div class="footer">
           <div class="footer__box">
-              <p>Copyright &copy; Your Website 2014</p>
+              <p>Smowee</p>
+              <a href="">link</a>
+          </div>
+
+          <div class="footer__box">
+              <p>Consumidor</p>
+              <a href="">link</a>
+          </div>
+
+          <div class="footer__box">
+              <p>Empresas</p>
+              <a href="">link</a>
+          </div>
+
+          <div class="footer__box">
+              <p>Redes Sociais</p>
+              <div class="footer__redes_sociais">
+                <div class="footer__icones__facebook">
+                  <i class="fa fa-facebook"></i>
+                </div>
+
+                <div class="footer__icones__twitter">
+                  <i class="fa fa-twitter"></i>
+                </div>
+
+                <div class="footer__icones__instagram">
+                  <i class="fa fa-instagram"></i>
+                </div>
+                
+              </div>
           </div>
       </div>
 
---
+++ application/views/home.php
@@ -0,0 +1,45 @@
+<div class="body">
+  <div class="body__section">
+    <div class="section__row">
+      <div class="home__button">
+        <a href="">
+          <!-- <img src="public/img/buttons" alt=""> -->
+        </a>
+      </div>
+
+      <div class="home__button">
+        <a href="">
+          <!-- <img src="public/img/buttons" alt=""> -->
+        </a>
+      </div>
+    </div>
+
+    <div class="home__button">
+      <a href="">
+        <!-- <img src="public/img/buttons" alt=""> -->
+      </a>
+    </div>
+  </div>
+
+  <div class="body__section">
+    <div class="home__button">
+      <a href="">
+        <!-- <img src="public/img/buttons" alt=""> -->
+      </a>
+    </div>
+
+    <div class="home__button">
+      <a href="">
+        <!-- <img src="public/img/buttons" alt=""> -->
+      </a>
+    </div>
+  </div>  
+
+  <div class="body__section">
+    <div class="home__button">
+      <a href="">
+        <!-- <img src="public/img/buttons" alt=""> -->
+      </a>
+    </div>
+  </div>  
+</div>
